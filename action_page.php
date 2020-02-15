9eafab1ab0ca693ece27c85d94bd63128a32e8f5
makamo66 <makamo66@hotmail.com>
2020-02-15 11:43:46 -0500

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Check Out Successful</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+
+You have successfully submitted your order.
+<br />
+<a href="cart.php">Back to shopping cart.</a>
+ 
+</body>
+</html>
